b5c2bee84581013ccb4433d8f8cc9cd8738d1581
JaeHyeokee <jaehyeok817@naver.com>
2024-07-27 18:46:54 +0900

    20240727 JWT

---
+++ Bt65_JWT/src/main/java/com/lec/spring/domain/User.java
@@ -0,0 +1,28 @@
+package com.lec.spring.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.sql.DataSourceDefinitions;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "JWT2_USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    private String role;  // "ROLE_MEMBER", "ROLE_MEMBER,ROLE_ADMIN"
+
+}
